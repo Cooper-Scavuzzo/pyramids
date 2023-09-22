7666d734ab0c47a8beb712df8dfa20e30df5a19c
Cooper-Scavuzzo <131704634+Cooper-Scavuzzo@users.noreply.github.com>
2023-09-22 08:46:31 -0500

    Add files via upload

---
+++ pyramids.py
@@ -0,0 +1,9 @@
+
+inputLength = eval(input("Enter an intiger between 1-15: "))
+#inputLength = height
+middleValue = 1
+
+for i in range(inputLength):
+    for i in range(inputLength):
+        print(" ", end="")
+  
